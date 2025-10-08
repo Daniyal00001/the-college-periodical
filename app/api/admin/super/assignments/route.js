0ae752eb8ce25d387d49990a74253d6ba2461179
Muhammad Daniyal Tallat <daniyaltallat0@gmail.com>
2025-10-08 12:25:30 -0700

    deployment issues

--- app/api/admin/super/assignments/route.js
+++ app/api/admin/super/assignments/route.js
@@ -29,6 +29,7 @@
 //     return NextResponse.json({ error: "Database error" }, { status: 500 })
 //   }
 // }
+export const dynamic = "force-dynamic";
 
 import { NextResponse } from "next/server"
 import db from "@/lib/db"
